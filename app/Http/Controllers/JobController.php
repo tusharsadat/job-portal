164b571f48d61a23215ff95b0063ee9d434502cd
tushar-sadat <tusharsadat30@gmail.com>
2024-10-03 13:10:28 +0600

    display related job data on single job page

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Job;
 use Illuminate\Http\Request;
 
@@ -9,9 +10,21 @@ class JobController extends Controller
 {
     public function index($id)
     {
-
+        // Find the job
         $singleJob = Job::find($id);
 
-        return view('/jobs.singleJob', compact('singleJob'));
+        // Get jobs related to the same category, excluding the current job
+        $relatedJobs = Job::where('category_id', $singleJob->category_id)
+            ->where('id', '!=', $singleJob->id)
+            ->take(5) // Limit to 5 related jobs
+            ->get();
+
+        // Count jobs related to the same category, excluding the current job
+        $relatedJobCount = Job::where('category_id', $singleJob->category_id)
+            ->where('id', '!=', $singleJob->id)
+            ->count();
+
+
+        return view('/jobs.singleJob', compact('singleJob', 'relatedJobs', 'relatedJobCount'));
     }
 }
