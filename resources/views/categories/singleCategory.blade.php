a4b4315540f64a7f3f8809f7c39d26e29e72d56e
tushar-sadat <tusharsadat30@gmail.com>
2024-10-06 01:01:59 +0600

    implement if condition under category no job found show a message

--- resources/views/categories/singleCategory.blade.php
+++ resources/views/categories/singleCategory.blade.php
@@ -28,29 +28,35 @@
             </div>
 
             <ul class="job-listings mb-5">
+                @if ($jobs->count() > 0)
+                    @foreach ($jobs as $job)
+                        <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
+                            <a href="{{ route('single-job', $job->id) }}"></a>
+                            <div class="job-listing-logo">
+                                <img src="{{ $job->image }}" alt="Free Website Template by Free-Template.co"
+                                    class="img-fluid">
+                            </div>
 
-                @foreach ($jobs as $job)
-                    <li class="job-listing d-block d-sm-flex pb-3 pb-sm-0 align-items-center">
-                        <a href="{{ route('single-job', $job->id) }}"></a>
-                        <div class="job-listing-logo">
-                            <img src="{{ $job->image }}" alt="Free Website Template by Free-Template.co"
-                                class="img-fluid">
-                        </div>
+                            <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
+                                <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
+                                    <h2>{{ $job->job_title }}</h2>
+                                    <strong>{{ $job->company_name }}</strong>
+                                </div>
+                                <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
+                                    <span class="icon-room"></span> {{ $job->region }}
+                                </div>
+                                <div class="job-listing-meta">
+                                    <span class="badge badge-danger">{{ $job->job_type }}</span>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                @else
+                    <div class="alert alert-info" id="alert-info">
+                        Under this category no job post available
+                    </div>
+                @endif
 
-                        <div class="job-listing-about d-sm-flex custom-width w-100 justify-content-between mx-4">
-                            <div class="job-listing-position custom-width w-50 mb-3 mb-sm-0">
-                                <h2>{{ $job->job_title }}</h2>
-                                <strong>{{ $job->company_name }}</strong>
-                            </div>
-                            <div class="job-listing-location mb-3 mb-sm-0 custom-width w-25">
-                                <span class="icon-room"></span> {{ $job->region }}
-                            </div>
-                            <div class="job-listing-meta">
-                                <span class="badge badge-danger">{{ $job->job_type }}</span>
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
             </ul>
             {{ $jobs->links() }}
         </div>
