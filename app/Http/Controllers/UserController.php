cc4dec817022759d724ac07ad494e538ebc9df46
tushar-sadat <tusharsadat30@gmail.com>
2024-10-06 21:22:22 +0600

    implement profile page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function profile() {}
+    public function profile()
+    {
+        $profile = User::find(Auth::user()->id);
+        return view('users.profile', compact('profile'));
+    }
 }
